26408724e672fbb4aa2f3ac70c1b2d674a26c0d9
6010110458 <6010110458@R301-mini07.local>
2020-10-07 17:50:30 +0700

    implement Project Display + onTap

--- lib/pages/lyrics_data.dart
+++ lib/pages/lyrics_data.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/project_list_cubit.dart';
+import '../config/colors_theme.dart';
+import '../config/colors_theme.dart';
+import '../project_entity.dart';
+
+class LyricsDataParam{
+  final int selectedDataIdx;
+  LyricsDataParam(this.selectedDataIdx);
+}
 
 class LyricsDataPage extends StatefulWidget {
+  final int selectedDataIdx;
+  LyricsDataPage(this.selectedDataIdx);
   @override
   _LyricsDataPageState createState() => _LyricsDataPageState();
 }
@@ -8,6 +21,19 @@ class LyricsDataPage extends StatefulWidget {
 class _LyricsDataPageState extends State<LyricsDataPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocBuilder<ProjectListCubit, List<SongProject>>(
+      builder: (context, state){
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(state[widget.selectedDataIdx].songName,
+              style: TextStyle(color: RectoNoteColors.colorPrimary, fontSize: 25),
+              ),
+          ),
+          body: Column(children: [
+            
+          ],),
+        );
+      }
+    );
   }
 }
