99ad6e38f9989d5a6b2e01a3779d016c1e624a07
6010110458 <6010110458@R301-mini07.local>
2020-10-14 19:58:12 +0700

    implement gridline (240-438chk2end)

--- lib/widgets/piano_roll_view.dart
+++ lib/widgets/piano_roll_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class PianoRollGrid extends StatelessWidget {
   final int octaveSize;
   int tableRowNum;
   PianoRollGrid(this.octaveSize){
-    tableRowNum = octaveSize * 12;
+    this.tableRowNum = octaveSize * 12;
   }
   
   @override
@@ -15,13 +16,25 @@ class PianoRollGrid extends StatelessWidget {
         children: [
           Table(border: TableBorder.all(width: 0.35),
             defaultColumnWidth: FixedColumnWidth(25),
-            children: List<TableRow>.filled(tableRowNum, TableRow(
-              children: List<Container>.filled(20, Container(
-                height: 24,
-                
-              ))
-            ))
-          ),
+            children: List<TableRow>.generate(tableRowNum,(i) => 
+            (i % 12 == 0) ? 
+              TableRow(
+                children: List<Container>.generate(64, (i) => Container(
+                  height: 24,
+                  child:  Text(( i % 8 == 0) ?  "| ${i ~/8}->\n|" : "" , style: TextStyle(fontSize: 11, height: 0.1),),
+                ))
+              )
+              :
+              TableRow(
+                children: List<Container>.generate(64, (i) => Container(
+                  height: 24,
+                  child: Text(( i % 8 == 0) ?  "|\n|" : ""  , style: TextStyle(fontSize: 11, textBaseline: TextBaseline.ideographic),),
+                ))
+              ) 
+            ),
+
+          )
+          
         ],
       ),
     );
