d384120f7369db7c4d1b81021c3e08fb7e2dbf8c
6010110458 <6010110458@R301-mini07.local>
2020-10-14 17:59:04 +0700

    init piano_view

--- lib/widgets/piano_view.dart
+++ lib/widgets/piano_view.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
 
 class PianoView extends StatelessWidget {
+  final pianoList = List<String>.generate(108, (i){
+      String temp; 
+      switch(i % 12){
+        case 0: temp = "C"; break;
+        case 1: temp = "C#";break;
+        case 2: temp = "D";break;
+        case 3: temp = "D#";break;
+        case 4: temp = "E";break;
+        case 5: temp = "F";break;
+        case 6: temp = "F#";break;
+        case 7: temp = "G";break;
+        case 8: temp = "G#";break;
+        case 9: temp = "A";break;
+        case 10: temp = "A#";break;
+        case 11: temp = "B";break;
+      }
+      return temp + "${i ~/ 12}";
+  });
   @override
   Widget build(BuildContext context) {
-    return SliverGrid.count(
-      crossAxisCount: 1,
+    return Column(children:  pianoList.reversed.map<Widget>( (note) =>
+          Container(
+            width: 50,
+            color: (note.contains("#")) ? Colors.black : Colors.white,
+            padding: EdgeInsets.all(5),
+            child: Text(note, style: TextStyle(color: (note.contains("#")) ? Colors.white : Colors.black),),
+          )
+      ).toList(),
+     
+        
+      
     );
   }
 }
